2b4a426c34cdb60641572b9ea18c7378274ebddf
ryongrau <spankleberry@gmail.com>
2014-04-11 15:44:13 -0400

    refactoring
    like the title says

--- contentscript.js
+++ contentscript.js
@@ -11,8 +11,25 @@ jQuery.expr[':'].regex = function(elem, index, match) {
     return regex.test(jQuery(elem)[attr.method](attr.property));
 }
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab?sender.tab.url :
+                request.greeting + ' do it');
+	//populateFields(request.greeting)
+	
+    if (request.greeting == "init publish"){
+		 $(':regex(href,workflow)').first().each(function(){
+			console.log('link:' + $(this).attr('href'));
+			window.location.replace('https://cms.doe.gov' + $(this).attr('href') + '?publish=immediately')
+		 });
+	}
+	sendResponse({farewell: "goodbye"});
+  });
+
 $( document ).ready(function() {
     console.log( "immediate publish ready!" );
+	
+	
 	if ($.url().param('publish')==='immediately'){
 		$(':regex(href,immediate\%20publish)').first().each(function(){
 			console.log('link:' + $(this).attr('href'));
@@ -20,31 +37,13 @@ $( document ).ready(function() {
 		 });
 	}
 	if ($.url().param('publish')==='finally'){
-		console.log('finally:');
-		$('#edit-submit').click();
+		console.log('?publish=finally:');
 		$('#edit-submit').trigger('click');
-		$('#edit-submit').trigger('mouseup');
-		$('#edit-submit').trigger('submit');
-	} else {
-		console.log('not final?  Really?');
-	}
+	} 
+
 });
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab?sender.tab.url :
-                request.greeting + ' do it');
-	//populateFields(request.greeting)
-	
-    if (request.greeting == "init publish")
-	
-	 $(':regex(href,workflow)').first().each(function(){
-		console.log('link:' + $(this).attr('href'));
-		window.location.replace('https://cms.doe.gov' + $(this).attr('href') + '?publish=immediately')
-	 });
-		
-      sendResponse({farewell: "goodbye"});
-  });
+
 
   
   
